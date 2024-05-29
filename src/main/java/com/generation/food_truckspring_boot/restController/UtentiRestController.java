99b16ce43732d3171440225fb81971f4961a7816
Mirko Di Franco <120678275+mirko-di-franco@users.noreply.github.com>
2024-05-29 20:26:37 +0200

    [fix] fix delete utente

--- src/main/java/com/generation/food_truckspring_boot/restController/UtentiRestController.java
+++ src/main/java/com/generation/food_truckspring_boot/restController/UtentiRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -109,6 +110,25 @@ public class UtentiRestController {
 			return new ResponseEntity<>(utenteDTO, HttpStatus.OK);
 		}
 			
+	}
+	
+	
+	
+	
+	//ELIMINAZIONE UTENTE
+	@DeleteMapping("/{idUtente}")
+	public ResponseEntity<Utenti> cancellaUtente(@PathVariable("idUtente") long idUtente){
+		try {
+			Optional<Utenti> utenteOptional = utentiServ.ricercaUtente(idUtente);
+			if(utenteOptional.isEmpty()) {
+				return new ResponseEntity<Utenti>(new Utenti(), HttpStatus.NOT_FOUND);
+			}else {
+				utentiServ.eliminaUtente(utenteOptional.get());
+				return new ResponseEntity<Utenti>(new Utenti(), HttpStatus.OK);
+			}
+		}catch(Exception e) {
+			return new ResponseEntity<Utenti>(new Utenti(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 	
